6dbe88d3174bfc336614a63aefef2e374cb771b8
carlton <64127681+notcarlton@users.noreply.github.com>
2022-11-28 06:16:09 -0700

    initial commit

---
+++ JiayiLauncher/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JiayiLauncher;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+	[DllImport("dwmapi.dll", PreserveSig = true)]
+	private static extern int DwmSetWindowAttribute(IntPtr hWnd, int attr, ref bool attrValue, int attrSize);
+	
+	public MainWindow()
+	{
+		InitializeComponent();
+		
+		SourceInitialized += DarkTitlebar;
+
+		var services = new ServiceCollection();
+		services.AddWpfBlazorWebView();
+#if DEBUG
+		services.AddBlazorWebViewDeveloperTools();
+#endif
+		Resources.Add("services", services.BuildServiceProvider());
+	}
+
+	private void DarkTitlebar(object? sender, EventArgs e)
+	{
+		var windowHelper = new WindowInteropHelper(this);
+		var value = true;
+		DwmSetWindowAttribute(windowHelper.Handle, 20, ref value, Marshal.SizeOf(value));
+	}
+}

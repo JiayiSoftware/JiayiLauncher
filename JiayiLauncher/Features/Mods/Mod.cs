45634a7cad026faa8f4fcdbf2be4edd7674ca033
carlton <64127681+notcarlton@users.noreply.github.com>
2023-02-16 11:57:37 -0700

    start on mod collections

---
+++ JiayiLauncher/Features/Mods/Mod.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JiayiLauncher.Features.Mods;
+
+public class Mod
+{
+	public string? Name { get; set; }
+	public string? Path { get; set; }
+	public List<string> SupportedVersions { get; set; } = new() { "Any" };
+}
---
+++ JiayiLauncher/Features/Mods/ModCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JiayiLauncher.Features.Mods;
+
+public class ModCollection
+{
+	public string BasePath { get; set; } = string.Empty;
+	public List<Mod> Mods { get; set; } = new();
+
+	private ModCollection() // nobody should be calling this
+	{
+		
+	}
+
+	public static ModCollection Create(string path)
+	{
+		throw new NotImplementedException();
+	}
+}

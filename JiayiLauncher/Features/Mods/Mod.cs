a25896a61e8d67ea301f987960b76e77ddb0380c
carlton <64127681+notcarlton@users.noreply.github.com>
2023-02-16 14:04:44 -0700

    more work done on mod collection

--- JiayiLauncher/Features/Mods/Mod.cs
+++ JiayiLauncher/Features/Mods/Mod.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace JiayiLauncher.Features.Mods;
 
 public class Mod
 {
-	public string? Name { get; set; }
-	public string? Path { get; set; }
-	public List<string> SupportedVersions { get; set; } = new() { "Any" };
+	public string Name { get; set; }
+	public string Path { get; set; }
+	public List<string> SupportedVersions { get; set; }
+
+	public Mod(string name, string path, List<string>? supportedVersions = null)
+	{
+		Name = name;
+		Path = path;
+		SupportedVersions = supportedVersions ?? new List<string> { "Any" };
+	}
+
+	public void SaveMetadata(ModCollection collection)
+	{
+		var filename = System.IO.Path.GetFileName(Path);
+		var modRelativePath = Path.Replace(collection.BasePath, string.Empty);
+		var metadataPath = System.IO.Path.Combine(collection.BasePath, ".jiayi", modRelativePath, filename + ".mod");
+		File.WriteAllText(metadataPath, $"{Name}\nat {Path}\nWorks on {string.Join(", ", SupportedVersions)}");
+	}
 }
--- JiayiLauncher/Features/Mods/ModCollection.cs
+++ JiayiLauncher/Features/Mods/ModCollection.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace JiayiLauncher.Features.Mods;
 
@@ -15,6 +18,43 @@ public class ModCollection
 
 	public static ModCollection Create(string path)
 	{
-		throw new NotImplementedException();
+		// path can either be an empty directory or one with mods in it already
+		// if it's empty, we'll create a new mod collection
+		
+		Directory.CreateDirectory(path);
+		Directory.CreateDirectory(Path.Combine(path, ".jiayi"));
+		File.WriteAllText(Path.Combine(path, ".jiayi", "README.txt"), 
+			"This folder contains metadata for all mods in this collection. Don't mess with it unless you know what you're doing.");
+		
+		var collection = new ModCollection
+		{
+			BasePath = path
+		};
+
+		var folders = Directory.GetDirectories(path);
+		var files = Directory.GetFiles(path);
+		if (folders.Length == 1 && folders[0].EndsWith(".jiayi") && files.Length == 0)
+		{
+			// empty directory
+			return collection;
+		}
+		
+		// create metadata for all mods in the directory
+		foreach (var folder in folders)
+		{
+			var files2 = Directory.GetFiles(folder);
+			if (files2.Length == 0) continue;
+
+			foreach (var file in files2)
+			{
+				var mod = new Mod(Path.GetFileName(folder), file);
+				// the user can give it a good name and supported versions later
+				mod.SaveMetadata(collection);
+				collection.Mods.Add(mod);
+				Debug.WriteLine($"Added mod {mod.Name} at {mod.Path}");
+			}
+		}
+		
+		return collection;
 	}
 }

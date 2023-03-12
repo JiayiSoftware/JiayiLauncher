0c06c1425f8725c607e1daddef623171568a093c
carlton <64127681+notcarlton@users.noreply.github.com>
2023-03-12 13:29:55 -0600

    start on game info

---
+++ JiayiLauncher/Features/Bridge/Minecraft.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace JiayiLauncher.Features.Bridge;
+
+public static class Minecraft
+{
+	private static AppDiagnosticInfo? _minecraftApp;
+
+	public static async Task<string> GetVersion()
+	{
+		if (_minecraftApp == null)
+		{
+			var info = await AppDiagnosticInfo.RequestInfoForPackageAsync("Microsoft.MinecraftUWP_8wekyb3d8bbwe");
+			if (info == null || info.Count == 0) return "Minecraft not found";
+			_minecraftApp = info[0];
+		}
+		
+		var version = _minecraftApp.AppInfo.Package.Id.Version;
+		return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+	}
+}

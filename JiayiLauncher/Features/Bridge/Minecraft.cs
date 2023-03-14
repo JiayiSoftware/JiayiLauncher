2fe595997cd368265aaa0431abf38132b30c34a3
carlton <64127681+notcarlton@users.noreply.github.com>
2023-03-14 17:09:08 -0600

    stuf

--- JiayiLauncher/Features/Bridge/Minecraft.cs
+++ JiayiLauncher/Features/Bridge/Minecraft.cs
@@ -6,18 +6,25 @@ namespace JiayiLauncher.Features.Bridge;
 
 public static class Minecraft
 {
-	private static AppDiagnosticInfo? _minecraftApp;
-
+	private static async Task<AppDiagnosticInfo?> GetPackage()
+	{
+		var info = await AppDiagnosticInfo.RequestInfoForPackageAsync("Microsoft.MinecraftUWP_8wekyb3d8bbwe");
+		if (info == null || info.Count == 0) return null;
+		return info[0];
+	}
+	
 	public static async Task<string> GetVersion()
 	{
-		if (_minecraftApp == null)
-		{
-			var info = await AppDiagnosticInfo.RequestInfoForPackageAsync("Microsoft.MinecraftUWP_8wekyb3d8bbwe");
-			if (info == null || info.Count == 0) return "Minecraft not found";
-			_minecraftApp = info[0];
-		}
-		
-		var version = _minecraftApp.AppInfo.Package.Id.Version;
+		var minecraftApp = await GetPackage();
+		if (minecraftApp == null) return "Unknown";
+		var version = minecraftApp.AppInfo.Package.Id.Version;
 		return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+	}
+
+	public static async Task Open()
+	{
+		var minecraftApp = await GetPackage();
+		if (minecraftApp == null) return;
+		await minecraftApp.LaunchAsync();
 	}
 }

6af2e951cb1a6dcefc8d50c98b2518030429e025
carlton <64127681+notcarlton@users.noreply.github.com>
2023-04-15 10:10:08 -0600

    why are colors being so weird

--- JiayiLauncher/Appearance/ThemeManager.cs
+++ JiayiLauncher/Appearance/ThemeManager.cs
@@ -1,6 +1,14 @@
-﻿namespace JiayiLauncher.Appearance;
+﻿using System;
+using System.IO;
+
+namespace JiayiLauncher.Appearance;
 
 public static class ThemeManager
 {
+	private static string _themePath = Path.Combine(Environment.CurrentDirectory, "wwwroot", "css", "theme.css");
 	
+	public static void Sync()
+	{
+		
+	}
 }

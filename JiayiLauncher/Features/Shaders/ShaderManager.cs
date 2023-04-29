e9def441b14c18808fdad73c51666a5e2496a206
carlton <64127681+notcarlton@users.noreply.github.com>
2023-04-28 22:21:12 -0600

    :older_man:

--- JiayiLauncher/Features/Shaders/ShaderManager.cs
+++ JiayiLauncher/Features/Shaders/ShaderManager.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using JiayiLauncher.Features.Game;
 using JiayiLauncher.Settings;
 using JiayiLauncher.Utils;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace JiayiLauncher.Features.Shaders;
 
@@ -44,7 +45,7 @@ public static class ShaderManager
 			File.Copy(file, Path.Combine(backupPath, fileName), true);
 		}
 		
-		Log.Write(nameof(ShaderManager), "Backed up vanilla shaders");
+		Log.Write(nameof(ShaderManager), $"Backed up vanilla shaders for version {await PackageData.GetVersion()}");
 	}
 	
 	public static async Task DeleteBackupShaders()
@@ -53,25 +54,77 @@ public static class ShaderManager
 		var path = Path.Combine(JiayiSettings.Instance!.ShadersPath, "Vanilla", version);
 		if (Directory.Exists(path)) Directory.Delete(path, true);
 		
-		Log.Write(nameof(ShaderManager), "Deleted backup shaders");
+		Log.Write(nameof(ShaderManager), $"Deleted backup shaders for version {version}");
+	}
+
+	public static async Task RestoreVanillaShaders()
+	{
+		var info = await PackageData.GetPackage();
+		if (info == null) return;
+		
+		var installPath = info.AppInfo.Package.InstalledPath;
+		var path = Path.Combine(installPath, "data", "renderer", "materials");
+		if (!Directory.Exists(path)) return;
+		
+		var version = await PackageData.GetVersion();
+		var backupPath = Path.Combine(JiayiSettings.Instance!.ShadersPath, "Vanilla", version);
+		if (!Directory.Exists(backupPath)) return;
+		
+		foreach (var file in Directory.GetFiles(backupPath))
+		{
+			var fileName = Path.GetFileName(file);
+			File.Copy(file, Path.Combine(path, fileName), true);
+		}
+		
+		Log.Write(nameof(ShaderManager), $"Restored vanilla shaders for version {version}");
 	}
 
 	public static void UpdateShaders()
 	{
+		Shaders.Clear();
+		AppliedShaders.Clear();
+		
 		if (!Directory.Exists(JiayiSettings.Instance!.ShadersPath))
 		{
-			JiayiSettings.Instance.ShadersPath =
-				Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-					"JiayiLauncher", "Shaders");
+			var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+				"JiayiLauncher", "Shaders");
+			
+			JiayiSettings.Instance.ShadersPath = path;
+			Directory.CreateDirectory(path);
+			Directory.CreateDirectory(Path.Combine(path, "Applied"));
 			
 			JiayiSettings.Instance.Save();
 		}
-		
+
 		var files = Directory.GetFiles(JiayiSettings.Instance.ShadersPath, "*.zip");
 		Shaders.AddRange(files.Select(Path.GetFileNameWithoutExtension)!);
+		
+		var applied = Directory.GetFiles(Path.Combine(JiayiSettings.Instance.ShadersPath, "Applied"), "*.zip");
+		AppliedShaders.AddRange(applied.Select(Path.GetFileNameWithoutExtension)!);
+		
+		Log.Write(nameof(ShaderManager), 
+			$"Updated shaders list. Found {Shaders.Count} shaders, {AppliedShaders.Count} of which are applied.");
 	}
 
-	public static async Task ApplyShader(string shader)
+	public static async Task AddShader(IBrowserFile file)
+	{
+		var path = Path.Combine(JiayiSettings.Instance!.ShadersPath, file.Name);
+		if (!path.EndsWith(".zip")) return;
+		
+		await using var stream = File.Create(path);
+		await file.OpenReadStream(524288000L).CopyToAsync(stream);
+		
+		Log.Write(nameof(ShaderManager), $"Added shader {file.Name}");
+		UpdateShaders();
+	}
+
+	private static async Task ApplyShader(string shader)
+	{
+		Directory.CreateDirectory(Path.Combine(JiayiSettings.Instance!.ShadersPath, "Applied"));
+		
+	}
+
+	public static async Task ApplyShaders()
 	{
 		
 	}

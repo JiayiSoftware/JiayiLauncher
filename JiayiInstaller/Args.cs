a680e4e514406307ec82babc8306d53935aa5dcd
carlton <64127681+notcarlton@users.noreply.github.com>
2023-05-04 08:37:50 -0600

    spaces in args

--- JiayiInstaller/Args.cs
+++ JiayiInstaller/Args.cs
@@ -7,7 +7,8 @@ public class Args
 	public Args()
 	{
 		var args = Environment.GetCommandLineArgs();
-		_args = args.Length > 1 ? args.Skip(1).Aggregate((a, b) => $"{a} {b}") : string.Empty;
+		// separate args with | so we can have spaces in them
+		_args = args.Length > 1 ? args.Skip(1).Aggregate((a, b) => $"{a}|{b}") : string.Empty;
 	}
 	
 	public bool Empty => string.IsNullOrEmpty(_args);
@@ -23,9 +24,9 @@ public class Args
 		var index = _args.IndexOf(fullFlag, StringComparison.Ordinal);
 		if (index == -1) return string.Empty;
 		
-		// start index + length of flag + 1 for space
+		// start index + length of flag + 1 for the argument separator
 		var startIndex = index + fullFlag.Length + 1;
-		var endIndex = _args.IndexOf(' ', startIndex);
+		var endIndex = _args.IndexOf('|', startIndex);
 		if (endIndex == -1) endIndex = _args.Length; // probably the last command
 		
 		return _args[startIndex..endIndex];

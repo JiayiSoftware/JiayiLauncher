7cf27950bf57ebdfe9cff31260ca35d412acc92e
carlton <64127681+notcarlton@users.noreply.github.com>
2023-06-07 11:32:28 -0600

    1.20.0.1 was 1.20.1.0 for some reason

--- JiayiLauncher/Features/Game/PackageData.cs
+++ JiayiLauncher/Features/Game/PackageData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.Management.Deployment;
 using Windows.System;
 using JiayiLauncher.Settings;
@@ -28,10 +29,28 @@ public static class PackageData
 		// the game does it weird
 		var major = version.Major;
 		var minor = version.Minor;
-		// actual build number does not include the last 2 digits
-		var build = version.Build.ToString()[..(version.Build.ToString().Length - 2)];
-		// the last number of the build number is the revision
-		var revision = version.Build.ToString()[^1];
+		
+		// weird edge case here
+		var build = version.Build.ToString();
+		char revision;
+		if (build.Length != 1)
+		{
+			build = build[..(version.Build.ToString().Length - 2)];
+			// the last number of the build number is the revision
+			revision = version.Build.ToString()[^1];
+		}
+		else
+		{
+			// for some reason 1.20.0.1's build and revision are swapped (1.20.1.0)
+			// i sure hope mojang just changed how versions work and this isn't a one time thing
+			if (version == new PackageVersion(1, 20, 1, 0))
+				return "1.20.0.1";
+			
+			// when mojang does the silly and makes the build number 1 digit
+			// the revision is literally the revision
+			revision = version.Revision.ToString()[^1];
+		}
+		
 		return $"{major}.{minor}.{build}.{revision}";
 	}
 

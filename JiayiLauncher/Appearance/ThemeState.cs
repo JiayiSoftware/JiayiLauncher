d266a429e8f13f51304bde22facb4e20d745b7d0
carlton <64127681+notcarlton@users.noreply.github.com>
2023-12-31 19:07:48 -0700

    no!!!

--- JiayiLauncher/Appearance/ThemeState.cs
+++ JiayiLauncher/Appearance/ThemeState.cs
@@ -15,7 +15,7 @@ public class ThemeState
 
     public static readonly string RootPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "wwwroot");
 
-    public static string ThemePath => Path.Combine(RootPath, "themes", JiayiSettings.Instance.Theme ?? ".local/default", "theme.css");
+    public static string ThemePath => Path.Combine(RootPath, "themes", JiayiSettings.Instance?.Theme ?? ".local/default", "theme.css");
 
     // If no CssBuilder, it sets it to an empty CssBuilder, which then ends up defaulting all the values in JiayiSettings.cs
     public ThemeState(CssBuilder? themeStyles = null) { ThemeStyles = themeStyles ?? new CssBuilder(); }

e203fc32b00e8fa97ac5051273ac6ed5f4502578
phase <64127681+phasephasephase@users.noreply.github.com>
2024-10-25 09:18:31 -0600

    What is JiayiLauncher even about :sob:

--- JiayiLauncher/Utils/JiayiTasks.cs
+++ JiayiLauncher/Utils/JiayiTasks.cs
@@ -10,6 +10,7 @@ public class JiayiTask
 	public Task Task { get; set; }
 	public string Name { get; set; }
 	public string FromPage { get; set; }
+	public bool ToastShown { get; set; }
 	
 	private readonly CancellationTokenSource _cts = new();
 	private readonly TaskFactory _factory;
@@ -17,6 +18,7 @@ public class JiayiTask
 	public JiayiTask(Task task, string name, string fromPage)
 	{
 		Task = task;
+		Name = name;
 		FromPage = fromPage;
 		_factory = new TaskFactory(_cts.Token);
 	}
@@ -36,59 +38,48 @@ public class JiayiTasks
 {
 	private readonly List<JiayiTask> _tasks = new();
 
-	private readonly Log _log = Singletons.Get<Log>();
-
-	public JiayiTasks()
+	public JiayiTasks(NavigationManager navigation, IToastService toastService)
 	{
-		var mainPage = (MainPage?)Application.Current!.MainPage;
-		if (mainPage == null)
+		navigation.LocationChanged += (_, e) =>
 		{
-			_log.Write(nameof(BlazorBridge), "MainPage was null", Log.LogLevel.Error);
-			return;
-		}
-
-		var dispatched = mainPage.BlazorWebView.TryDispatchAsync(sp =>
-		{
-			var navigation = sp.GetRequiredService<NavigationManager>();
-			navigation.LocationChanged += (_, e) =>
+			foreach (var task in _tasks)
 			{
-				foreach (var task in _tasks)
+				if (!e.Location.Contains(task.FromPage))
 				{
-					if (e.Location.Contains(task.FromPage))
-					{
-						var bridge = Singletons.Get<BlazorBridge>();
-
-						var toastParams = new ToastParameters()
-							.Add(nameof(JiayiToast.Level), ToastLevel.Info)
-							.Add(nameof(JiayiToast.Title), task.Name)
-							.Add(nameof(JiayiToast.Content), new RenderFragment(builder =>
-							{
-								builder.OpenElement(0, "p");
-								builder.AddContent(1, "This task is running in the background.");
-								builder.CloseElement();
-							}));
-						
-						bridge.ShowToast(toastParams, settings =>
+					if (task.ToastShown) continue;
+					
+					var toastParams = new ToastParameters()
+						.Add(nameof(JiayiToast.Level), ToastLevel.Info)
+						.Add(nameof(JiayiToast.Title), task.Name)
+						.Add(nameof(JiayiToast.Content), new RenderFragment(builder =>
 						{
-							settings.Timeout = 3;
-							settings.ShowCloseButton = false;
-						});
-					}
+							builder.OpenElement(0, "p");
+							builder.AddContent(1, "This task is running in the background.");
+							builder.CloseElement();
+						}));
+						
+					toastService.ShowToast<JiayiToast>(toastParams, settings =>
+					{
+						settings.Timeout = 3;
+					});
+					
+					task.ToastShown = true;
 				}
-			};
-		}).Result;
-
-		if (!dispatched)
-			_log.Write(nameof(JiayiTasks), "Some background tasks may not work properly.", Log.LogLevel.Warning);
+				else
+				{
+					task.ToastShown = false;
+				}
+			}
+		};
 	}
 
 	public JiayiTask AddTask(Task task, string name, string fromPage)
 	{
 		var jiayiTask = new JiayiTask(task, name, fromPage);
-		_tasks.Add(jiayiTask);
 		
 		// remove task from list when it's done
-		task.ContinueWith(_ => _tasks.Remove(jiayiTask));
+		//task.ContinueWith(_ => _tasks.Remove(jiayiTask));
+		_tasks.Add(jiayiTask);
 		
 		return jiayiTask;
 	}
